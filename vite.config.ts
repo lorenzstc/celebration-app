6716e24043585468d456098d9c69e65174fa2ae6
ga48him <lorenz.steckhan@tum.de>
2025-02-14 00:16:53 +0100

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/celebration-app/",  // Wichtig für GitHub Pages!
+  base: "/celebrationapp/",  // Wichtig für GitHub Pages!
   build: {
     outDir: "docs", // Hier ändern wir den Build-Ordner auf docs/
   }
